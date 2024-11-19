3aaec570f00a6a53e0bdf39d92d6b2e4c5a8f042
Gustavo Voltani von Atzingen <gustavo.von.atzingen@gmail.com>
2024-11-19 16:45:29 -0300

    working version of parser

--- data_model.py
+++ data_model.py
@@ -1,6 +1,33 @@
 from dataclasses import dataclass, field
 
 @dataclass
+class TMDL:
+    """
+    Estrutura de dados para o modelo de dados (DataModelSchema) retirados do json
+    do pbi para alimentar o jinja2
+    """
+    description: str = ''
+    element: str = ""
+    properties: list = field(default_factory=lambda : [])
+    calculation: str = ''
+    @classmethod
+    def create(cls, description, element, properties, calculation=''):
+        return cls(description, element, properties, calculation)
+    
+    def __str__(self):
+        prop_str = "\n".join(
+            f"\t{'-' * 10}\n\tDescription: {prop.description}\n\tElement: {prop.element}\n\tProps: {prop.properties}\n\tCalcs: {prop.calculation}"
+            if isinstance(prop, TMDL) else f"\t{prop}"
+            for prop in self.properties
+        )
+        return (
+            f"Description: \n{self.description}\n"
+            f"Element: \n{self.element}\n"
+            f"Properties:\n{prop_str}\n"
+            f"Calculation: {self.calculation}\n{'-' * 20}"
+        )
+
+@dataclass
 class Table:
     """
     Estrutura de dados para tabelas regulares (regular tables) retirados do json
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hello !")
+from data_model import *
+from parse_tmdl import TMLDParser
+
+file_content = 'pbip/PNP_Publicada_dev.SemanticModel/definition/tables/NaturezaDespesa.tmdl'
+file_content = 'pbip/PNP_Publicada_dev.SemanticModel/definition/model.tmdl'
+file_content = 'pbip/PNP_Publicada_dev.SemanticModel/definition/tables/dimCurso.tmdl'
+
+tmdl = TMLDParser('pbip/PNP_Publicada_dev.pbip')
+tables = tmdl.parse_all_tables()
+
+for table_name, table_data in tables.items():
+    print(f"Table Name: {table_name}")
+    # for t in table_data:
+    #     print(t)
+
